b58c8d648d8231b48be0c6912bed37153b4968a2
Justin Schellenberg P <jrschellenberg@gmail.com>
2019-09-29 23:39:44 -0500

    Adding Typed.js to project and improving CTA

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,18 @@ import '../sass/main.scss';
 import 'jquery/dist/jquery.slim.min';
 import 'popper.js/dist/umd/popper.min';
 import 'bootstrap/dist/js/bootstrap.min';
-
 import SmoothScroll from 'smooth-scroll';
+import Typed from 'typed.js';
 
 const scroll = new SmoothScroll('a[href*="#"]');
+
+const typed = new Typed('.typedjs', {
+  strings: ["Quality.", "Speed Optimization.", "User-Centered Design.", "SEO.", "CRO."],
+  shuffle: true,
+  loop: true,
+  backDelay: 3000,
+  backSpeed: 120,
+  typeSpeed: 240
+});
+
 
