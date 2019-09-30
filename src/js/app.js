841ed17fab62ae10a497a9e466be21dd371a4fec
Justin Schellenberg P <jrschellenberg@gmail.com>
2019-09-29 20:16:05 -0500

    Add Smooth scroll and some SEO

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
-// import '../sass/dependencies/bootstrap-4.3.1.min.scss';
 import '../sass/main.scss';
 
 import 'jquery/dist/jquery.slim.min';
 import 'popper.js/dist/umd/popper.min';
 import 'bootstrap/dist/js/bootstrap.min';
 
+import SmoothScroll from 'smooth-scroll';
 
+const scroll = new SmoothScroll('a[href*="#"]');
 
-// If need to use Newer Javascript to be transpiled, throw it in here.
