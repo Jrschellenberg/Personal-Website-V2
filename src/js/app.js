448ea4fc5a916ebd5c0dca2cc9256248d7e70169
Justin Schellenberg P <jrschellenberg@gmail.com>
2020-05-10 09:25:31 -0500

    Add logic to Hide Email and Address and Phone

--- src/js/app.js
+++ src/js/app.js
@@ -18,3 +18,32 @@ const typed = new Typed('.typedjs', {
 });
 
 
+const getStr = (arr) => {
+  return arr.join('');
+}
+
+grecaptcha.ready(function() {
+  grecaptcha.execute('6LcawLYUAAAAAEFtdXwAeCEbDPc3pQOvqkHHcm7d', {action: 'homepage'}).then(function(token) {
+
+    const arr = ['jr','schell','enberg@', 'gm', 'ail.c', 'om'];
+    const addressArr = ['191 Li', 'ndmere Drive<br>', 'Win', 'nipeg, M', 'B R3P 2R5<', 'br>Canada'];
+    const phoneArr = ['(20', '4) 2', '90-0', '973'];
+
+    const formSpree = document.querySelector('#hidden_form')
+    formSpree.setAttribute('action', `https://formspree.io/${getStr(arr)}`);
+
+    const emailAddress = document.querySelector('#emailAddress');
+
+    emailAddress.setAttribute('href', `mailto:${getStr(arr)}`)
+    emailAddress.innerText = getStr(arr);
+
+    const address = document.querySelector('#address');
+    address.innerHTML = getStr(addressArr);
+
+    const phone = document.querySelector('#phone');
+    phone.innerText = getStr(phoneArr);
+  });
+});
+
+
+
